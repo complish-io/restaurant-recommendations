9d01698f63e36d8050933a8add11f9cbc2b76413
Penn Wu <pennwu2@gmail.com>
2020-09-05 22:31:44 -0400

    Add Eslint, Prettier, precommit hook (#3)

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    'react-native/react-native': true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/prop-types': 0,
+  },
+  plugins: ['@typescript-eslint', 'react-native'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+};
